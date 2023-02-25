ae08d3ecda0da040f0095cedd075da7bff27a462
Javier Paez <jhpaez0806@gmail.com>
2023-02-25 18:43:21 -0300

    Adding sold overlay

--- components/products/productCard.js
+++ components/products/productCard.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import { React, useState } from 'react';
+import { Card, Badge } from 'react-bootstrap';
 
 const ProductCard = ({ product }) => {
+  const [sold, setSold] = useState(product.available === "NO");
+
   return (
-      <Card>
+    <Card className={`product-card ${sold ? 'sold' : ''}`}>
+      <div className="product-image">
         <Card.Img variant="top" src={product.hasImage !== "NO" ? `/images/catalog/${product.code}.jpg` : ''} alt="SIN IMAGEN" />
-        <Card.Body>
-          <Card.Title>
-            {product.available === 'NO' && <span className="text-muted">[VENDIDO]</span>}
-            {product.name}
-          </Card.Title>
-          <Card.Text style={{ width: '18rem', textDecoration: product.available === 'NO' ? 'line-through' : 'none' }}>
-            {product.priceARS && <text>Precio: {parseFloat(product.priceARS).toFixed(0)}$</text>}
-          </Card.Text>
-        </Card.Body>
-      </Card>
+        {sold && (
+          <div className="sold-overlay">Vendido</div>
+        )}
+      </div>
+      <Card.Body>
+        <Card.Title>
+          {product.available === 'NO' && <Badge>VENDIDO</Badge>}
+          {product.name}
+        </Card.Title>
+        <Card.Text style={{ width: '18rem', textDecoration: product.available === 'NO' ? 'line-through' : 'none' }}>
+          {product.priceARS && <text>Precio: {parseFloat(product.priceARS).toFixed(0)}$</text>}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
