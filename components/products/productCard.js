78ba82538d359d9c5fe6a6f6adba0e8e8c68f43f
Javier Paez <jhpaez0806@gmail.com>
2023-02-25 16:48:38 -0300

    Basic catalog working, missing details

--- components/products/productCard.js
+++ components/products/productCard.js
@@ -3,15 +3,22 @@ import { Card } from 'react-bootstrap';
 
 const ProductCard = ({ product }) => {
   return (
-    <Card>
-      <Card.Img variant="top" src={`/images/catalog/${product.code}.jpg`} />
-      <Card.Body>
-        <Card.Title>{product.name} {product.available === 'NO' && <text className="text-muted">vendido</text>}</Card.Title>
-        <Card.Text style={{ width: '18rem', textDecoration: product.available === 'NO' ? 'line-through' : 'none' }}>
-          {product.priceARS && <text>Precio: {parseFloat(product.priceARS).toFixed(0)}$</text>}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <>{
+      product.hasImage !== "NO" &&
+      <Card>
+        <Card.Img variant="top" src={product.hasImage !== "NO" ? `/images/catalog/${product.code}.jpg` : ''} alt="SIN IMAGEN" />
+        <Card.Body>
+          <Card.Title>
+            {product.available === 'NO' && <span className="text-muted">[VENDIDO]</span>}
+            {product.name}
+          </Card.Title>
+          <Card.Text style={{ width: '18rem', textDecoration: product.available === 'NO' ? 'line-through' : 'none' }}>
+            {product.priceARS && <text>Precio: {parseFloat(product.priceARS).toFixed(0)}$</text>}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    }
+    </>
   );
 };
 
