dcca28078ce3a026b7d91346406dcf30ac4d8931
Javier Paez <jhpaez0806@gmail.com>
2024-05-13 23:24:53 -0300

    Adding a new section for new chapter

--- pages/about.js
+++ pages/about.js
@@ -13,7 +13,7 @@ export default function About() {
             <Image src="/images/Fuji_trip.jpg" className="rounded" width={300} height={200} />
           </div>
         </div>
-        <div className="row">
+        <div className="row align-items-center">
           <div className="col-lg-6 px-6 mx-auto">
             <p className="font-italic text-muted mb-4">
               El nombre Nanfuen significa Viento del sur y fue elegido por mi Oyakata Nobuichi Urushibata, despues de preguntarle si podria comenzar mi propio estudio de bonsai.
@@ -53,6 +53,23 @@ export default function About() {
             </div>
             <p className="font-italic text-muted mb-4">
               La situación de pandemia nos llevó a trasladar tanto ventas como taller a formato online, ampliando también los productos que hoy podemos ofrecer.
+            </p>
+          </div>
+        </div>
+        <div className="row align-items-center">
+          <div className="col-lg-6 px-6 mx-auto">
+            <div className="  d-flex justify-content-end">
+              <Image src="/images/new_oyakata.png" className="rounded" width={300} height={400} />
+            </div>
+            <p className="font-italic text-muted mb-4">
+              Ya en el 2023 logramos volver a donde todo empezo, a Taisho-en, ya con Taiga Urushibata como nuestro nuevo Oyakata, aquella persona que admiraba desde el primer viaje a partir de ese dia tendria el honor que corrigiera mi trabajo y poder tener esas charlas como las que tenia con su padre, las alegrias que te da el arte del bonsai!
+            </p>
+          </div>
+        </div>
+        <div className="row align-items-center">
+          <div className="col-lg-6 order-2 order-lg-1">
+            <p className="font-italic text-muted mb-4">
+              Hoy a casi 10 años del primer viaje que seria el puntapie inicial de esta aventura llamada Nanfuen, puedo decir que he tenido el honor de trabajar, estudiar y compartir con dos de las personas que mas admiro en bonsai. Pude trabajar arboles para exhibiciones, ver arboles evolucionar, poder comenzar a dar clases y comenzar un sueño al que llamo Nanfuen.
             </p>
           </div>
         </div>
