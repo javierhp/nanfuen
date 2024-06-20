4cea741fe7ff3ad85aa23b8b6ffcf88499ae3767
Javier Paez <jhpaez0806@gmail.com>
2024-06-20 20:25:03 -0300

    Fixing how to byy column issue

--- components/howToBuy.js
+++ components/howToBuy.js
@@ -3,10 +3,12 @@ import { Form, Row, Col, Button } from 'react-bootstrap';
 
 function HowToBuy({ product }) {
   return (
-      <Row>
+    <Row>
+      <Col>
         <h5>Los precios se calculan con la cotizacion 'blue' del dolar</h5>
         <h5>Para comprar escribinos a nanfuen.bonsai@gmail.com con el / los productos que te gustaria adquirir</h5>
-      </Row>
+      </Col>
+    </Row>
   );
 }
 
